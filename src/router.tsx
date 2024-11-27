aa25e0f36c6f08cb28180dea34e6ceb5df920b01
secgyu <abc2232002@gmail.com>
2024-11-27 21:08:06 +0900

    feat: DevicePage 구현

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import MainPage from "./pages/MainPage";
 import './reset.css'
+import DevicePage from "./pages/DevicePage";
 
 const router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/device" element={<DevicePage />} />
       </Routes>
     </BrowserRouter>
   );
